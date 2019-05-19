c3db192d7a8ea0a736e57ce85f0fa353d1b1216c
Pruthvi Sumanth Kakani <48427671+sumanthpk@users.noreply.github.com>
2019-05-18 20:26:02 -0700

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+
+  string s1;
+
+  cout << "Please enter your name\n";
+
+  cin >> s1 ;
+
+  cout << "hello" + s1;
+  
+
+}
